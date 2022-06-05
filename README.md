ae010d0cf307d26d651bb4412e2925d0d3134dd3
Joeh <joeh@alchemist.wtf>
2022-06-05 13:19:41 +0100

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sobal Contribute
+
+Welcome to Sobals Contribute repo. 
+
+This platform is a fork of `turtlecoin/trtl-issues`.
+
+## Purpose
+
+This board lists github `issues` from all sobal public repos, welcoming internal or external contribution.
+
+## Viewing
+
+You can view this site live at https://contribute.sobal.fi/(https://contribute.sobal.fi/)
---
+++ netlify/functions/get-github-issues.ts
@@ -0,0 +1,61 @@
+import fetch from "node-fetch";
+
+const handler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        return {
+            statusCode: 405,
+            body: 'Method Not Allowed.',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                Allow: 'GET'
+            }
+        };
+    }
+
+    const { repo } = event.queryStringParameters;
+
+    try {
+        let issues;
+
+        if (repo) {
+
+            const issueRes = await fetch(`https://api.github.com/repos/${repo}/issues`, {
+                headers: {
+                    Authorization: 'token '+process.env.GITHUB_TOKEN,
+                },
+            });
+            
+            issues = await issueRes.json();
+
+        } else {
+
+            const res = await fetch('https://api.github.com/orgs/sobalfinance/repos?sort=updated', {
+                headers: {
+                    Authorization: 'token '+process.env.GITHUB_TOKEN,
+                },
+            });
+
+            issues = await res.json();
+
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(issues),
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+    } catch (error) {
+        console.log('error', error);
+        return {
+            statusCode: 400,
+            statusMessage: JSON.stringify(error),
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+    }
+};
+
+module.exports = { handler };
